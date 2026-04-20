b32122fe2af933da918912a8ec8cc2103ac7d769
Work <artem_gerasin@mail.ru>
2026-04-20 21:41:46 +0400

    Дполнил

--- src/MasterShawerman.java
+++ src/MasterShawerman.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
 
-public class Shawerman {
+public class MasterShawerman {
+    public static void main(String[] args) {
+        String name = "Ибрагим";
+        String position ="ШаурманШейх";
+        int rateOneDay = 800;
+        int workDay = 21;
+        int benefits = 3000;
+        int fineForLavash = 600;
+        int priceOneShaverma = 230;
+        int allSaleShaverma = 672;
+
+        int mounthSalary = rateOneDay * workDay; //считаем зарплату без штрафов и премий
+        int mounthSalaryResult = (mounthSalary + benefits) - fineForLavash; //зарплата с учётом надбавок и вычетов
+        int amountOfProfit = priceOneShaverma*allSaleShaverma;
+
+        System.out.println("Сотрудник - " + name + "\nДолжность - " + position);
+        System.out.println("Оплата за смены - " + mounthSalary);
+        System.out.println("Премия - " + benefits + "\nШтраф - " + fineForLavash);
+        System.out.println("Итоговая зарплата = " + mounthSalaryResult);
+        System.out.println("Шаур-выручка = " + amountOfProfit);
+    }
 }
