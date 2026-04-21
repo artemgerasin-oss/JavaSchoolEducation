7366a631899279a4616ec9b63e3d79921e8f31a6
Work <artem_gerasin@mail.ru>
2026-04-21 10:44:22 +0400

    Дполнил

---
+++ src/BussinessCoach.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+public class BussinessCoach {
+    public static void main (String [] args) {
+
+        Random random = new Random();
+        int ageVisitor = random.nextInt(90);
+        double amountInTheAccount = random.nextDouble() * 100_000;
+
+        boolean hasInvite = false;
+        boolean InBlackList = false;
+
+        boolean already18Years = ageVisitor >= 18;
+        boolean canPresent = (already18Years && (hasInvite || amountInTheAccount > 50_000) && !InBlackList);
+        double voluntaryContribution = amountInTheAccount * 7.5 / 100;
+
+        System.out.println("Возраст посетителя: " + ageVisitor);
+        System.out.println("Сумма на счете (руб): " + amountInTheAccount);
+        System.out.println("Допуск на мероприятие: " + canPresent);
+        System.out.println("Добровольно-принудительный взнос (руб): " + voluntaryContribution);
+
+    }
+}
